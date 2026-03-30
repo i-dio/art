cbd6910e245a0291b7f8e487384955dee9b7bf20
i-dio <anthony.weisse@outlook.fr>
2026-03-30 15:33:22 +0200

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,1 +1,28 @@
-r=Math.random,S=Math.sin,p=[];for(i=12;i--;)d=document.body.appendChild(document.createElement('div')),d.className='p',d.innerText='♟♞♝♜♛♚'[r()*6|0],z=.5+r(),d.sx=.2+r()*.8*z,d.sy=.2+r()*.8*z,d.ax=5+r()*10*z,d.ay=5+r()*10*z,d.o=r()*6,d.style.cssText=`left:${r()*innerWidth}px;top:${r()*innerHeight}px;font-size:${1+z}em;opacity:${.3+z/2}`,p.push(d);addEventListener('scroll',()=>{for(i=12;i--;){e=p[i];s=scrollY*i*.01;e.style.transform=`translate(${e.ax*S(s*e.sx+e.o)}px,${e.ay*S(s*e.sy+e.o+1.57)}px)`}})
+r=Math.random,S=Math.sin,p=[];
+for(i=12;i--;) {
+  d=document.body.appendChild(document.createElement('div'));
+  d.className='p';
+  d.innerText='♟♞♝♜♛♚'[r()*6|0];
+  z=.5+r();                     // profondeur
+  d.sx=.2+r()*.8*z; d.sy=.2+r()*.8*z;
+  d.ax=5+r()*10*z; d.ay=5+r()*10*z;
+  d.o=r()*6;
+  d.style.cssText=`left:${r()*innerWidth}px;top:${r()*innerHeight}px;font-size:${1+z}em;opacity:${.3+z/2}`;
+  p.push(d);
+
+  // interaction touch / click
+  d.addEventListener('pointerdown',e=>{
+    d.style.transform=`translate(${d.ax*S(scrollY*d.sx+d.o)+10}px,${d.ay*S(scrollY*d.sy+d.o+1.57)+10}px)`;
+  });
+  d.addEventListener('pointerup',e=>{
+    d.style.transform=`translate(${d.ax*S(scrollY*d.sx+d.o)}px,${d.ay*S(scrollY*d.sy+d.o+1.57)}px)`;
+  });
+}
+
+addEventListener('scroll',()=>{
+  for(i=12;i--;){
+    e=p[i];
+    s=scrollY*i*.01;
+    e.style.transform=`translate(${e.ax*S(s*e.sx+e.o)}px,${e.ay*S(s*e.sy+e.o+1.57)}px)`;
+  }
+});
